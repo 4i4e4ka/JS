c9e77b067db7fa1a9175c9747833833bd64ed0e5
4i4e4ka <68158196+4i4e4ka@users.noreply.github.com>
2021-03-29 20:16:22 +0300

    feat: unit 3
    Watch: Task 11, Task 15

---
+++ Unit 3/js/script3.js
@@ -0,0 +1,323 @@
+// Task 1
+// При нажатии кнопки b - 1 срабатывает функция f1.
+// Функция должна прочитать содержимое i - 1 и сравнить его
+// с числом 4(сравнение ==).
+// Результат сравнения - true или false выведите в out - 1.
+
+// const button = document.querySelector(".b-1");
+// let input = document.querySelector(".i-1");
+
+// button.onclick = () => {
+//     let num = +input.value;
+
+//     if (num===4) {
+//         console.log("true");
+//     }
+
+//     else {
+//         console.log("false");
+//     }
+// }
+ 
+
+
+// Task 2
+// Даны две переменные a21 и a22.При нажатии кнопки b - 2, запускается функция f2.
+// Функция должна сравнить переменные с помощью if else и вывести в out - 2 число,
+// которое больше.
+// Вариант равенства переменных не рассматриваем.
+
+// let a21 = 15;
+// let a22 = 24;
+
+// const button = document.querySelector(".b-2");
+// button.onclick = ()=> {
+//     if (a21 > a22) {
+//         console.log(a21);
+//     }
+//     else {
+//        console.log(a22);
+//     }
+// }
+
+// Task 3
+// Даны 2 input - i-31 и i-32, оба - input[type=number]. 
+// При нажатии кнопки b - 3 срабатывает функция f3.
+// Функция должна прочитать содержимое i - 31 и i - 32 в переменные и сравнить их,
+//     вывести в out - 3 большее число.
+
+// Проведите самостоятельный тест работы, введите пары чисел 4 и 9, 9 и 22, 5 и 111.
+
+// const button = document.querySelector(".b-3");
+// let inputOne = document.querySelector(".i-31");
+// let inputTwo = document.querySelector(".i-32");
+
+    
+// button.onclick = () => {
+  
+// let one = +inputOne.value;
+// let two = +inputTwo.value;
+    
+//     if (one > two) {
+//         console.log(one);
+//     } else
+//         {console.log(two)}
+    
+// }
+
+// Task 4
+// Создайте на странице input[type = number] с классом i - 4,
+// куда пользователь может ввести год своего рождения.
+// Есть кнопка b - 4 которая запускает функцию f4.
+// Функция должна вывести в.out - 4 число 1 если пользователю больше или равно 18 лет,
+// и 0 если меньше.
+
+// const button = document.querySelector(".button");
+// const age = document.querySelector(".age");
+
+// button.onclick = () => {
+//     let num = +age.value;
+//     if (num >= 18) {
+//         console.log(1);
+//     }
+//     else {
+//         console.log(0);
+//     }
+// }
+
+
+// Task 5
+// Создайте на странице input[type = number] с классом i - 5,
+// куда пользователь может ввести число.Есть кнопка b - 5
+// которая запускает функцию f5.Функция должна вывести в.out - 5 символ m
+// если число меньше нуля, 0 если число равно нулю и 1 если больше.
+
+
+// const button = document.querySelector(".b-5");
+// let input = document.querySelector(".i-5");
+
+// button.onclick = () => {
+//     let num = +input.value;
+
+//     if (num <0) {
+//         console.log("m");
+//     }
+//     else if (num===0){
+//         console.log(0);
+//     }
+
+//     else {console.log(1)}
+// }
+
+// Task 6
+// Создайте на странице input[type = number] с классом i - 6, куда пользователь может
+// ввести число.Есть кнопка b - 6 которая запускает функцию f6.
+// Функция должна вывести в.out - 6 слово even если число четное
+// и odd если нечетное.Для проверки четности используется целочисленный
+// остаток от деления на 2(оператор %).
+// Если остаток равен нулю - четное, нет - нечетное.
+
+// const button = document.querySelector(".b-6");
+// let input = document.querySelector(".i-6");
+// button.onclick = () => {
+//     let num = +input.value;
+//     if (num % 2===0) {
+//         console.log("even");
+//     } else {
+//         console.log("odd");
+//     }
+// }
+
+
+// Task 7
+// Даны 2 input - i - 71 и i - 72, оба - input[type = number].
+// При нажатии кнопки b - 7 срабатывает функция f7.
+// Функция должна число из i - 71 возвести в степень i - 72,
+// вывести результат в out - 7. Для возведения в степень можно использовать **,
+// или Math.pow.
+
+// const button = document.querySelector(".b-7");
+// let inputOne = document.querySelector(".i-71");
+// let inputTwo = document.querySelector(".i-72");
+
+// button.onclick = () => {
+//     let a = +inputOne.value;
+//     let b = +inputTwo.value;
+
+//     console.log(Math.pow (a,b));
+// }
+
+// Task 8
+// Дан select s - 8, который содержит 3 значения: 1, 2, 3.
+// Дана кнопка b - 8. При ее нажатии срабатывает функция f8.
+// Функция должна получить выбранное в select число,
+// потом с помощью switch case сравнить его поочередно с ‘1’, ‘2’, ‘3’.
+// И если число выбрано - 1, то вывести в out - 8 строку one,
+// если 2 - two, если 3 - three.
+
+
+// const button = document.querySelector("input");
+// let selected = document.querySelector(".select");
+
+// button.onclick = () => {
+//     let n = +selected.value;
+  
+
+//     switch (n) {
+//         case 1:
+//             console.log("one");
+//             break;
+//         case 2:
+//             console.log("two");
+//             break;
+//         case 3:
+//             console.log("three");
+//             break;
+//     }
+// }
+
+// Task 9
+// Создайте на странице input[type = number] с классом i - 9,
+// куда пользователь может ввести номер квартиры.
+// Есть кнопка b - 9 которая запускает функцию f9.
+// Функция должна вывести в.out - 9 номер подъезда,
+//     в котором находится квартира.
+
+// если от 1 до 32 - то вывести цифру 1
+// если от 33 до 43 - то вывести 2
+// если от 44 до 64 - то 3.
+// В противном случае, вывести 0.
+
+// const button = document.querySelector(".b-9");
+// const input = document.querySelector(".i-9");
+
+
+// button.onclick = () => {
+
+//     let num = input.value;
+//     num = Number(input.value);
+
+//     if (num >= 1 && num <= 32) {
+//         console.log(1);
+//     }
+
+//     else if 
+//     (num >= 33 && num <= 43) {
+//         console.log(2);
+//     }
+       
+//     else if (num >= 44 && num <= 64) {
+//         console.log(3);
+//     }
+
+//     else {
+//         console.log(0);
+//     }
+// }
+
+
+// Task 10
+// Дан select s-100. По нажатию кнопки, выведите value выбранного option в out-10.
+
+// const button = document.querySelector(".b-10");
+// let select = document.querySelector(".s-100");
+
+// button.onclick = () => {
+// let n = select.value;
+
+// console.log(n);
+// }
+
+// Task 11
+// Дан select s - 110. По изменению состояния select(событие onchange)
+// выведите value выбранного option в out - 11.
+
+
+// const select = document.querySelector(".s-110");
+// select.onchange = ()=> {
+//     console.log(select.value);
+// }
+
+
+// Task 12
+// Дан input i - 120. По нажатию кнопки получите значение из input в переменную,
+// а затем выведите в out - 12 typeof полученной переменной.
+// Typeof позволяет определить тип данных.Обратите внимание, данная задача уже решена,
+// нужно убрать комментарии и изучить работу.
+
+// const button = document.querySelector(".btn");
+// const inp = document.querySelector(".input");
+
+// button.onclick = () => {
+//     let result = +inp.value;
+//     console.log(typeof result);
+//    Привести полученную строку к числу  console.log(Number (result))
+ 
+
+// Task 13
+// Дан input i - 130. В отличие от предыдущего задания - input type number.
+// По нажатию кнопки получите значение из input в переменную, а затем выведите в out - 13
+// typeof полученной переменной.Typeof позволяет определить тип данных.
+// Если вы правильно все сделали - то удивительно, но тип данных будет string!
+// Подумайте почему так ?
+
+// Task 14
+// Дан input i - 141 и input - 142, type = number.
+// Дан select s - 143, который содержит две операции - +, -, *, / . 
+// Дана кнопка b - 14, при нажатии на которую срабатывает функция f14.
+// Функция выводит в out - 14 результат операций выбранной в 3 - м select к числам
+// введенным в первом и втором input.Например выбрано 1 13 +,
+// нужно вывести результат операции 1 + 13 т.е. 14.
+
+
+
+// const button = document.querySelector(".b-14");
+// const inpOne = document.querySelector(".i-141");
+// const inpTwo = document.querySelector(".i-142");
+// const select = document.querySelector(".s-143");
+
+// button.onclick = () => {
+//     let c = select.value;
+//     let a = Number(inpOne.value);
+//     let b = Number(inpTwo.value);
+    
+//     if (c === "+") {
+//         console.log(a + b);
+//     }  
+//     else if (c === "-") {
+//         console.log(a - b);
+//    }
+//     else if (c === "*") {
+//         console.log(a * b);
+//     }
+//      else if (c === "/") {
+//         console.log(a / b);
+//     }
+// }
+    
+    
+// Task 15
+// Дан select s - 151 и s - 152, каждый из которых содержит 1 и 0.
+// Дан select s - 153, который содержит две операции - && и || .
+// Дана кнопка b - 15, при нажатии на которую срабатывает функция f15.
+// Функция выводит в out - 15 результат логических операций выбранных в 3 select
+// к числам выбранным в первом и втором select.Например выбрано 1 1 &&,
+// нужно вывести результат операции 1 && 1 т.е. 1 или 0.
+
+    
+const button = document.querySelector(".b-15");
+const selectOne = document.querySelector(".s-151");
+const selectTwo = document.querySelector(".s-152");
+const selectThree = document.querySelector(".s-153");
+
+button.onclick = () => {
+    let a = selectOne.value;
+    let b = selectTwo.value;
+    let c = selectThree.value;
+
+    if (c==="&&") {
+        console.log(a && b);
+    } else if (c === "||") {
+        console.log(a || b);
+    } 
+}
